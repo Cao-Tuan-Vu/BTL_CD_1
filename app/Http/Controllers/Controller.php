a908a331742385c2cec809a3f7fedeffb6d4c575
Thawngs <thawngs@MacBook-Air-cua-Thawngs.local>
2026-04-14 16:08:42 +0700

    update Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,11 +2,19 @@
 
 namespace App\Http\Controllers;
 
+// Trait hỗ trợ kiểm tra phân quyền (authorize)
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+// Trait hỗ trợ validate dữ liệu request
 use Illuminate\Foundation\Validation\ValidatesRequests;
 
+// Controller trừu tượng (base controller)
+// Các controller khác sẽ kế thừa từ class này
 abstract class Controller
 {
+    // Sử dụng trait để gọi các hàm authorize như: $this->authorize()
     use AuthorizesRequests;
+
+    // Sử dụng trait để validate nhanh dữ liệu request
     use ValidatesRequests;
 }
